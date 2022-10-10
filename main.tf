784e3da31460521d846ae91fd3338d66a31b3870
viveky255 <viveksonofprasad@gmail.com>
2022-10-10 14:50:52 +0530

    updated

--- main.tf
+++ main.tf
@@ -90,10 +90,7 @@ data "aws_ami" "ubuntu" {
     name = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220912"]
   }
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
+ 
   
 }
 
